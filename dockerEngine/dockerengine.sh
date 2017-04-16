5c85565dd522491938812893840d8fea4ecc4414
root <root@localhost.localdomain>
2017-04-16 02:28:04 -0400

    Working dockerEngine script

--- dockerEngine/dockerengine.sh
+++ dockerEngine/dockerengine.sh
@@ -11,16 +11,16 @@ yum-config-manager --enable docker-ce-edge
 
 yum install docker-ce
 
-sed -i '/ExecStart=\/usr\/bin\/dockerd/c\ExecStart=/usr/bin/dockerd --selinux-enabled --tls=true --tlscacert=/root/ca.pem --tlscert=/root/server-cert.pem --tlskey=/root/server-key.pem -H tcp://0.0.0.0:2376' /usr/lib/systemd/system/docker.service
+sed -i '/ExecStart=\/usr\/bin\/dockerd/c\ExecStart=/usr/bin/dockerd --selinux-enabled --tls=true --tlscacert=/root/certs/ca.pem --tlscert=/root/certs/server-cert.pem --tlskey=/root/certs/server-key.pem -H tcp://0.0.0.0:2376' /usr/lib/systemd/system/docker.service
 
 systemctl disable firewalld
 
-echo '\nexport DOCKER_TLS_VERIFY="1"
+echo 'export DOCKER_TLS_VERIFY="1"
   export DOCKER_HOST="tcp://localhost:2376"
-  export DOCKER_CERT_PATH="/root"' >> ~/.bashrc
+  export DOCKER_CERT_PATH="/root/certs"' >> ~/.bashrc
 
 
-source .bashrc
+source ~/.bashrc
 
 systemctl daemon-reload
 
